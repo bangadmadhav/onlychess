60f82a47e010a159f00e21f24f9f0780054afb07
bangadmadhav <bangadmadhav@gmail.com>
2025-03-10 21:43:39 +0530

    Initial Setup of OnlyChess.org

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-xl'>Hello Madhav</h1>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Board.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Board = () => {
+  return (
+    <div className='chessboard | '>
+{/* 
+PLAYER DETAILS
+EVALUATION BAR | BOARD
+PLAYER DETAILS*/}
+    </div>
+  )
+}
+
+export default Board
